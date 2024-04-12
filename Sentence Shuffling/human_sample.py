93a96727398c438cf65f14ff021ae3f9ef7a690d
Alexa Gogoescu <gogoescu.alexa@gmail.com>
2024-04-12 15:44:23 -0700

    Update human_sample.py

--- Sentence Shuffling/human_sample.py
+++ Sentence Shuffling/human_sample.py
@@ -5,10 +5,10 @@ file = "/Users/goose/Documents/CPSC 448/Sentence Shuffling/shuffled_sent_data.js
 
 df = pl.read_ndjson(file)
 
-sample = list(df.sample(50, with_replacement=False))
+sample = df.sample(50, with_replacement=False)
 
 first = sample[:30]
 second = sample[-30:]
 
-first.write_ndjson("/Users/goose/Documents/CPSC 448/Sentence Shuffling/first sample.jsonl")
-first.write_ndjson("/Users/goose/Documents/CPSC 448/Sentence Shuffling/second sample.jsonl")
+first.write_ndjson("first sample.jsonl")
+first.write_ndjson("second sample.jsonl")
