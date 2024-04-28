b9f6679e0b896394dd5ff004a84bbfa2ed674be6
AlexaGogo <gogoescu.alexa@gmail.com>
2024-04-28 00:05:02 -0700

    attempt to calculate spearmanr

--- Sentence Shuffling/kendalltau.py
+++ Sentence Shuffling/kendalltau.py
@@ -1,8 +1,6 @@
 import numpy as np
-import scipy
-import pandas as pd
+import scipy as sp
 import polars as pl
-import csv
 
 def normalised_kendall_tau_distance(values1, values2):
     """Compute the Kendall tau distance."""
@@ -15,20 +13,30 @@ def normalised_kendall_tau_distance(values1, values2):
     ndisordered = np.logical_or(np.logical_and(a[i] < a[j], b[i] > b[j]), np.logical_and(a[i] > a[j], b[i] < b[j])).sum()
     return ndisordered / (n * (n - 1))
 
+
+## process first sample
 first = pl.read_csv("anno_data/first_answers.csv")
 
+## extract lists from annotation/label strings
 first = first.with_columns(
     pl.col('anno_label').str.json_decode().alias('anno_label'),
     pl.col('label').str.json_decode().alias('label')
 )
 
+## compute length of annotation/label for error checking
 first = first.with_columns(
     pl.struct(['anno_label']).map_elements(lambda x: len(x['anno_label'])).alias('anno_length'),
     pl.struct(['label']).map_elements(lambda x: len(x['label'])).alias('label_length')
 )
 
+## calculate kendall tau distance
 first = first.with_columns(
-    pl.struct(['anno_label', 'label']).map_elements(lambda x: normalised_kendall_tau_distance(x['anno_label'], x['label'])).alias('kendall_tau_distance')
+    pl.struct(['anno_label', 'label']).map_elements(lambda x: normalised_kendall_tau_distance(x['anno_label'], x['label'])).alias('kendall_tau_distance'),
+)
+
+## spearman correlation between length and kendall tau distance???
+first = first.with_columns(
+    pl.struct(['label_length', 'kendall_tau_distance']).map_elements(lambda x: sp.stats.spearmanr(x['label_length'], x['kendall_tau_distance'])).alias('length_corr')
 )
 
 
