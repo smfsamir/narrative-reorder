f093fa8d165736149d182706d05c0b67d7f2dc75
AlexaGogo <gogoescu.alexa@gmail.com>
2024-04-29 08:56:53 -0700

    misc edits and additions

--- Sentence Shuffling/kendalltau.py
+++ Sentence Shuffling/kendalltau.py
@@ -34,12 +34,6 @@ first = first.with_columns(
     pl.struct(['anno_label', 'label']).map_elements(lambda x: normalised_kendall_tau_distance(x['anno_label'], x['label'])).alias('kendall_tau_distance'),
 )
 
-## spearman correlation between length and kendall tau distance???
-first = first.with_columns(
-    pl.struct(['label_length', 'kendall_tau_distance']).map_elements(lambda x: sp.stats.spearmanr(x['label_length'], x['kendall_tau_distance'])).alias('length_corr')
-)
-
-
 second = pl.read_csv("anno_data/second_answers.csv")
 
 second = second.with_columns(
@@ -56,5 +50,36 @@ second = second.with_columns(
     pl.struct(['anno_label', 'label']).map_elements(lambda x: normalised_kendall_tau_distance(x['anno_label'], x['label'])).alias('kendall_tau_distance')
 )
 
-print(first)
-print(second)
+third = pl.read_csv("anno_data/third_answers.csv")
+
+third = third.with_columns(
+    pl.col('anno_label').str.json_decode().alias('anno_label'),
+    pl.col('label').str.json_decode().alias('label')
+)
+
+third = third.with_columns(
+    pl.struct(['anno_label']).map_elements(lambda x: len(x['anno_label'])).alias('anno_length'),
+    pl.struct(['label']).map_elements(lambda x: len(x['label'])).alias('label_length')
+)
+
+third = third.with_columns(
+    pl.struct(['anno_label', 'label']).map_elements(lambda x: normalised_kendall_tau_distance(x['anno_label'], x['label'])).alias('kendall_tau_distance')
+)
+
+
+
+## print(first)
+print(sp.stats.spearmanr(first['label_length'], first['kendall_tau_distance']))
+print(first.mean())
+
+## print(second.head())
+print(sp.stats.spearmanr(second['label_length'], second['kendall_tau_distance']))
+print(second.mean())
+
+## print(third.head())
+print(sp.stats.spearmanr(third['label_length'], third['kendall_tau_distance']))
+print(third.mean())
+
+""" first.write_ndjson("/Users/goose/Documents/Projects/CPSC 448/narrative-reorder/Sentence Shuffling/anno_data/first.csv")
+second.write_ndjson("/Users/goose/Documents/Projects/CPSC 448/narrative-reorder/Sentence Shuffling/anno_data/second.csv")
+third.write_ndjson("/Users/goose/Documents/Projects/CPSC 448/narrative-reorder/Sentence Shuffling/anno_data/third.csv") """
